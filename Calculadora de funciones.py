783dc7fb9548cd17c2ec79b92dbe8fb19d4d3b80
Maxix25 <maximiliano.besoain@gmail.com>
2020-10-22 08:53:20 -0300

    Mas Cambios

--- Calculadora de funciones.py
+++ Calculadora de funciones.py
@@ -1,18 +1,31 @@
 print("Calculadora de funciones")
-x1=int(input("Introduce el valor de x1: "))
-y1=int(input("Introduce el valor de y1: "))
-x2=int(input("Introduce el valor de x2: "))
-y2=int(input("Introduce el valor de y2: "))
-suma_y2_y1=y2-y1
-suma_x2_x1=x2-x1
-m=suma_y2_y1/suma_x2_x1
+while True:
+	try:
+		x1 = int(input("Introduce el valor de x1: "))
+		y1 = int(input("Introduce el valor de y1: "))
+		x2 = int(input("Introduce el valor de x2: "))
+		y2 = int(input("Introduce el valor de y2: "))
+		break
+	except ValueError:
+		print("Has introducido un valor no numerico")
+		continue
+
+resta_y2_y1 = y2 - y1
+resta_x2_x1 = x2 - x1
+m = resta_y2_y1 // resta_x2_x1
 print(f"El valor de la pendiente es: {m}")
 #y=f(x)=mx+n
-x=x1
-y=y1
-multiplicacion=m*x
-n=y-multiplicacion
+x = x1
+y = y1
+mx = m * x
+n = y - mx
 print(f"El valor de n es: {n}")
-f_de_x=int(input("Introduce el valor de el numero que te toco: "))
-funcion=m*f_de_x+n
-print(f"El valor de la funcion que te toco es: {funcion}")
+while True:
+	try:
+		f_de_x = int(input("Introduce el valor de f(x): "))
+		break
+	except ValueError:
+		print("Has introducido un valor no numerico")
+		continue
+funcion = m * f_de_x + n
+print(f"El valor de la funcion que introduciste es: {funcion}")
