2921e2bc852041ea47f4e8716ea8c7dd9ab36510
Maxix25 <maximiliano.besoain@gmail.com>
2020-10-22 09:06:18 -0300

    Modificacion de variables

--- Calculadora de funciones.py
+++ Calculadora de funciones.py
@@ -1,4 +1,4 @@
-print("Calculadora de funciones")
+run = True
 while True:
 	try:
 		x1 = int(input("Introduce el valor de x1: "))
@@ -10,22 +10,28 @@ while True:
 		print("Has introducido un valor no numerico")
 		continue
 
-resta_y2_y1 = y2 - y1
-resta_x2_x1 = x2 - x1
-m = resta_y2_y1 // resta_x2_x1
+m = (y2-y1) // (x2-x1)
 print(f"El valor de la pendiente es: {m}")
 #y=f(x)=mx+n
-x = x1
-y = y1
-mx = m * x
-n = y - mx
+mx = m * x1
+n = y1 - mx
 print(f"El valor de n es: {n}")
-while True:
+while run == True:
 	try:
-		f_de_x = int(input("Introduce el valor de f(x): "))
-		break
+                f_de_x = int(input("Introduce el valor de f(x): "))
+                funcion = m * f_de_x + n
+                print(f"El valor de la funcion que introduciste es: {funcion}")
+                while True:
+                    opcion = input("Quieres ir de nuevo? [Y/n]").lower()
+                    if opcion == "y" or opcion == "":
+                        continue
+                    elif opcion == "n":
+                        print("Saliendo del programa...")
+                        run = False
+                        break
+                    else:
+                        print("Has introducido una opcion no valida")
+                        continue
 	except ValueError:
 		print("Has introducido un valor no numerico")
 		continue
-funcion = m * f_de_x + n
-print(f"El valor de la funcion que introduciste es: {funcion}")
